35bff1f9322f64840fea583dcd4abdeef2fd1732
stalin670 <amitalvin01@gmail.com>
2025-12-14 11:20:16 +0530

    Implemented zustand states in flowCanvas

--- src/components/flow/FlowCanvas.tsx
+++ src/components/flow/FlowCanvas.tsx
@@ -8,11 +8,39 @@ import ReactFlow, {
 } from "reactflow";
 import "reactflow/dist/style.css";
 
+import { useFlowStore } from "@/store/flowStore";
+import TextNode from "../flow/nodes/TextNode";
+import ImageNode from "../flow/nodes/ImageNode";
+import LLMNode from "../flow/nodes/LLMNode";
+
+const nodeTypes = {
+    text: TextNode,
+    image: ImageNode,
+    llm: LLMNode,
+};
+
 export default function FlowCanvas() {
+    const {
+        nodes,
+        edges,
+        onNodesChange,
+        onEdgesChange,
+        onConnect,
+    } = useFlowStore();
     return (
         <div className="flex-1">
-            <ReactFlow>
-                <Background variant={BackgroundVariant.Dots} gap={16} size={1} />
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                nodeTypes={nodeTypes}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                fitView
+            >
+                <Background
+                    variant={BackgroundVariant.Dots}
+                    gap={16} size={1} />
                 <Controls />
                 <MiniMap />
             </ReactFlow>
