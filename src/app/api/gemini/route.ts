18d1c80e8a4f3a910bd5e09e5e8bfcfbc1aae0ae
stalin670 <amitalvin01@gmail.com>
2025-12-14 13:01:57 +0530

    implemented zod for safe checks

--- src/app/api/gemini/route.ts
+++ src/app/api/gemini/route.ts
@@ -1,40 +1,59 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { z } from "zod";
 import { NextResponse } from "next/server";
 
-console.log("Gemini route file loaded");
+const BodySchema = z.object({
+    text: z.string().min(1),
+});
+
+// console.log("Gemini route file loaded");
 
 export async function POST(req: Request) {
-    const apiKey = process.env.GEMINI_API_KEY;
 
-    console.log(apiKey);
-    if (!apiKey) {
+    try {
+        const body = BodySchema.parse(await req.json());
 
+        const apiKey = process.env.GEMINI_API_KEY;
+
+        // console.log(apiKey);
+        if (!apiKey) {
+
+            return NextResponse.json(
+                { error: "GEMINI_API_KEY is missing" },
+                { status: 500 }
+            );
+        }
+
+        // console.log(req.json());
+
+        const prompt = body.text;
+
+        if (!prompt) {
+            return NextResponse.json(
+                { error: "Prompt is required" },
+                { status: 400 }
+            );
+        }
+
+        const genAI = new GoogleGenerativeAI(apiKey);
+
+
+        const model = genAI.getGenerativeModel({
+            model: "gemini-2.5-flash"
+        });
+
+        const result = await model.generateContent(prompt);
+
+        return NextResponse.json({
+            output: result.response.text(),
+        });
+
+    } catch (error) {
         return NextResponse.json(
-            { error: "GEMINI_API_KEY is missing" },
+            { error: "Internal Server Error" },
             { status: 500 }
         );
     }
-    const { prompt } = await req.json();
-
-    if (!prompt) {
-        return NextResponse.json(
-            { error: "Prompt is required" },
-            { status: 400 }
-        );
-    }
-
-    const genAI = new GoogleGenerativeAI(apiKey);
-
-
-    const model = genAI.getGenerativeModel({
-        model: "gemini-2.5-flash"
-    });
-
-    const result = await model.generateContent(prompt);
-
-    return NextResponse.json({
-        output: result.response.text(),
-    });
 }
 
 export async function GET() {
