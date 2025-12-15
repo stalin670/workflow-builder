120e086cc14f3462935caf37d565024189472d5b
stalin670 <amitalvin01@gmail.com>
2025-12-16 05:22:02 +0530

    debug workflow save api

--- src/app/api/workflow/save/route.ts
+++ src/app/api/workflow/save/route.ts
@@ -5,13 +5,24 @@ import Workflow from "@/models/Workflow";
 export async function POST(req: Request) {
     const { name, nodes, edges } = await req.json();
 
+    console.log("Saving workflow:", { name, nodes, edges });
+
     await connectDB();
 
+    console.log("Coming before creating workflow");
     const workflow = await Workflow.create({
         name,
         nodes,
         edges,
     });
+    console.log("Coming after creating workflow");
+
+    await workflow.save();
+
+    if (!workflow) {
+
+        return NextResponse.json({ success: false }, { status: 500 });
+    }
 
     return NextResponse.json({ success: true, id: workflow._id });
 }
