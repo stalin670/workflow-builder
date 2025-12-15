0b264e38baa611ddfeb820234716416cfa8d8a30
stalin670 <amitalvin01@gmail.com>
2025-12-15 09:53:50 +0530

    Implemented reRoute logic after login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,19 @@
 
 import Sidebar from "@/components/sidebar/Sidebar";
 import FlowCanvas from "@/components/flow/FlowCanvas";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   const [collapsed, setCollapsed] = useState(false);
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    if (!userId) {
+      router.push("/login");
+    }
+  }, [router]);
   return (
     <div className="flex h-screen bg-[#0b0f1a] text-white">
       <button
